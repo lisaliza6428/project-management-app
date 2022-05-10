684c60eee872b2f58d1d8d454ea4c1dab2fc26a2
SiarheiTamkovich <mpg2000@mail.ru>
2022-05-10 13:38:16 +0300

    fix: a bug when adding an empty card

--- src/app/boards/components/board/board.component.ts
+++ src/app/boards/components/board/board.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+// eslint-disable-next-line import/named
+import { Component, OnInit } from '@angular/core';
 import { ListComponent } from '../list/list.component';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrls: ['./board.component.scss']
+  styleUrls: ['./board.component.scss'],
 })
-export class BoardComponent {
+export class BoardComponent implements OnInit {
 
   lists: ListComponent[] = [];
+
+  ngOnInit(): void {
+    this.addList();
+  }
 
   addList() {
     const newList = new ListComponent();
