37325521191cae071e81eaadde57c93c2f242a24
Elizaveta <elizavetaivanushenko@gmail.com>
2022-05-17 11:13:42 +0300

    refactor: fix path in case of 401 error

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -57,8 +57,7 @@ export class AuthService {
   }
 
   getUsers() {
-    this.http.get(BASE_URL + 'users').subscribe((value) => {
-      //console.log(value);
+    this.http.get(BASE_URL + 'users').subscribe(() => {
     });
   }
 
